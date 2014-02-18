b1d4a86563183c0ff01f7cc3845ba5d27306c98f
YashYash <yash.saxena1217@gmail.com>
2014-02-18 10:51:49 -0800

    another commit

--- shagabond.js
+++ shagabond.js
@@ -5,6 +5,7 @@ $("#aboutme").hide();
 $("#mymusic").hide();
 $("#into").hide();
 $("#instahide").hide();
+$("#aboutmepop").hide();
 	$("#btn1").hide();
 	$("#click").click(function(){
 			$("#into").fadeIn("slow");
@@ -58,5 +59,54 @@ $("#instahide").hide();
 		$("#makebooking").fadeIn("slow");
 		$("#mymusic").hide();		
 	});
+	$("#btn3").hide();
+	$("#john").click(function(){
+			$("#aboutmepop").fadeIn("slow");
+			$("#btn3").fadeIn("slow");
+	});
+	$("#abtme").click(function(){
+			$("#aboutmepop").fadeIn("slow");
+			$("#btn3").fadeIn("slow");
+	});
+	$("#btn3").click(function(){
+			$("#aboutmepop").fadeOut("slow");
+			$("#btn3").hide();
+	});
+	
+	var paragraph = document.getElementsByTagName('p')[0];
+
+function textEffect(animationName) {
+  var text = paragraph.innerHTML,
+		chars = text.length,
+		newText = '',
+		animation = animationName,
+		char,
+		i;
+
+	for (i = 0; i < chars; i += 1) {
+		newText += '<i>' + text.charAt(i) + '</i>';
+	}
+
+	paragraph.innerHTML = newText;
+
+	var wrappedChars = document.getElementsByTagName('i'),
+		wrappedCharsLen = wrappedChars.length,
+		j = 0;
+
+	function addEffect () {
+		setTimeout(function () {
+			wrappedChars[j].className = animation;
+			j += 1;
+			if (j < wrappedCharsLen) {
+				addEffect();
+			}
+		}, 100)
+	}
+
+	addEffect();
+};
+
+textEffect('fly-in-out');
+
 	
 });
